68ba88094fa0f9967431c64e247298c4523025f7
kouki485 <mwd3145@gmail.com>
2023-06-13 20:57:02 +0900

    fin

--- handson/app.js
+++ handson/app.js
@@ -3,4 +3,21 @@ var upBtn = document.getElementById('upBtn');
 var downBtn = document.getElementById('downBtn');
 
 
-var rotateValue = circle.style
+var rotateValue = circle.style.transform;
+// console.log(rotateValue);
+
+var rotateSum;
+
+
+upBtn.onclick = function(){
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
+
+downBtn.onclick = function(){
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
+
